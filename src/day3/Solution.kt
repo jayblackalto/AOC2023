a46af8e2c7b9ac39abf7f1bfef1c27cf73ae26c6
jaycarey <jaycarey@icloud.com>
2023-12-05 07:39:50 +0000

    Day 3 part 2.

--- src/day3/Solution.kt
+++ src/day3/Solution.kt
@@ -6,6 +6,7 @@ import readInput
 fun main() {
     fun String.isSymbol(x: Int) =
         x in indices && (!get(x).isDigit() && get(x) != '.')
+
     fun String.isGear(x: Int) =
         x in indices && get(x) == '*'
 
@@ -15,7 +16,7 @@ fun main() {
         return lines.any { l -> chars.any { c -> this.getOrNull(l)?.getOrNull(c) == true } }
     }
 
-    fun List<List<Boolean>>.findSymbols(start: Int, end: Int, lineNumber: Int): List<Pair<Int, Int>> {
+    fun List<List<Boolean>>.findSymbolCoordinates(start: Int, end: Int, lineNumber: Int): List<Pair<Int, Int>> {
         val lines = (lineNumber - 1)..(lineNumber + 1)
         val chars = (start - 1)..(end + 1)
         return lines.flatMap { l -> chars.mapNotNull { c -> if (this.getOrNull(l)?.getOrNull(c) == true) (l to c) else null } }
@@ -38,6 +39,29 @@ fun main() {
         }
     }
 
+    fun findGearRatios(symbols: List<List<Boolean>>, lineNumber: Int, line: String, ratiosByCoordinates: MutableMap<Pair<Int, Int>, MutableList<Int>>) {
+        var start = -1
+        line.forEachIndexed { charNum, c ->
+            if (!c.isDigit()) {
+                if (start > -1) {
+                    symbols.findSymbolCoordinates(start, charNum - 1, lineNumber).forEach {
+                        val ratios = ratiosByCoordinates.getOrPut(it) { mutableListOf() }
+                        ratios += line.substring(start, charNum).toInt()
+                    }
+                }
+                start = -1
+            } else {
+                if (start == -1) start = charNum
+            }
+        }
+        if (start > -1) {
+            symbols.findSymbolCoordinates(start, line.length, lineNumber).forEach {
+                val ratios = ratiosByCoordinates.getOrPut(it) { mutableListOf() }
+                ratios += line.substring(start).toInt()
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
         val symbols = input.map { line -> line.indices.map(line::isSymbol) }
         val serials = input.flatMapIndexed { lineNumber, line -> findSerials(symbols, lineNumber, line).toList() }
@@ -46,16 +70,18 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         val symbols = input.map { line -> line.indices.map(line::isGear) }
-        val serials = input.flatMapIndexed { lineNumber, line -> findSerials(symbols, lineNumber, line).toList() }
-        return serials.sum()
+        val coordinates = mutableMapOf<Pair<Int, Int>, MutableList<Int>>()
+        input.forEachIndexed { lineNumber, line -> findGearRatios(symbols, lineNumber, line, coordinates) }
+        coordinates.forEach { (k, v) -> println("$k -> $v") }
+        return coordinates.filter { it.value.size > 1 }.values.sumOf { it.reduce { acc, i -> acc * i } }
     }
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("day3/example1")
     check(part1(testInput) == 4361) { "${part1(testInput)} == 4361" }
 
-//    val testInput2 = readInput("day2/example2")
-//    check(part2(testInput2) == 2286) { "${part2(testInput2)} == 2286" }
+    val testInput2 = readInput("day3/example2")
+    check(part2(testInput2) == 467835) { "${part2(testInput2)} == 467835" }
 
     val input = readInput("day3/test")
     part1(input).println()
